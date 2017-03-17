d2e083ac924c74e3661d4d4c2aca27893295e36a
Rottana <rottanaly@ymail.com>
2017-03-17 14:18:26 +0100

    Initialization

---
+++ app/src/main/java/m2dl/mobe/android/project/challengeandroid/Activity/MainActivity.java
@@ -0,0 +1,42 @@
+package m2dl.mobe.android.project.challengeandroid.Activity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import m2dl.mobe.android.project.challengeandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStart;
+    private Button buttonScore;
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonScore = (Button) findViewById(R.id.buttonScore);
+        buttonScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
